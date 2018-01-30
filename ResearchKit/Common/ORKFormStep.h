ae303065156f1248f8fb61e5c3a284d0abc153ee
Yaseen Majeed <majeed@trialx.com>
2018-01-30 13:16:08 +0530

    added manual initialisers for ORKFormItem

--- ResearchKit/Common/ORKFormStep.h
+++ ResearchKit/Common/ORKFormStep.h
@@ -63,38 +63,38 @@ NS_ASSUME_NONNULL_BEGIN
 
 ORK_CLASS_AVAILABLE
 @interface ORKFormStep : ORKStep
-
-/**
- Returns an initialized form step using the specified identifier, title, and text.
- 
- @param identifier    The string that identifies the step (see `ORKStep`).
- @param title         The title of the form (see `ORKStep`).
- @param text          The text shown immediately below the title (see `ORKStep`).
- 
- @return As initialized form step object.
- */
+    
+    /**
+     Returns an initialized form step using the specified identifier, title, and text.
+     
+     @param identifier    The string that identifies the step (see `ORKStep`).
+     @param title         The title of the form (see `ORKStep`).
+     @param text          The text shown immediately below the title (see `ORKStep`).
+     
+     @return As initialized form step object.
+     */
 - (instancetype)initWithIdentifier:(NSString *)identifier
                              title:(nullable NSString *)title
                               text:(nullable NSString *)text;
-
-/**
- Additional text to display for the step in a localized string at the bottom of the view.
- 
- The footnote is displayed in a smaller font below the continue button. It is intended to be used
- in order to include disclaimer, copyright, etc. that is important to display in the step but
- should not distract from the main purpose of the step.
- */
-@property (nonatomic, copy, nullable) NSString *footnote;
-
-/**
- The array of items in the form.
- 
- A form step that contains no items is considered invalid and an exception will be thrown
- when it is presented.
- */
-@property (nonatomic, copy, nullable) NSArray<ORKFormItem *> *formItems;
-
-@end
+    
+    /**
+     Additional text to display for the step in a localized string at the bottom of the view.
+     
+     The footnote is displayed in a smaller font below the continue button. It is intended to be used
+     in order to include disclaimer, copyright, etc. that is important to display in the step but
+     should not distract from the main purpose of the step.
+     */
+    @property (nonatomic, copy, nullable) NSString *footnote;
+    
+    /**
+     The array of items in the form.
+     
+     A form step that contains no items is considered invalid and an exception will be thrown
+     when it is presented.
+     */
+    @property (nonatomic, copy, nullable) NSArray<ORKFormItem *> *formItems;
+    
+    @end
 
 
 /**
@@ -113,120 +113,154 @@ ORK_CLASS_AVAILABLE
  */
 ORK_CLASS_AVAILABLE
 @interface ORKFormItem : NSObject <NSSecureCoding, NSCopying>
-
-/**
- Returns an initialized form item using the specified identifier, title, and answer format.
- 
- @param identifier    The string that identifies the form item, which should be unique within the form step.
- @param text          The text displayed as a prompt for the form item's question.
- @param answerFormat  The answer format for the form item.
- 
- @return An initialized form item.
- */
+    
+    /**
+     Returns an initialized form item using the specified identifier, title, and answer format.
+     
+     @param identifier    The string that identifies the form item, which should be unique within the form step.
+     @param text          The text displayed as a prompt for the form item's question.
+     @param answerFormat  The answer format for the form item.
+     
+     @return An initialized form item.
+     */
 - (instancetype)initWithIdentifier:(NSString *)identifier
                               text:(nullable NSString *)text
                       answerFormat:(nullable ORKAnswerFormat *)answerFormat;
-
-/**
- Returns an initialized form item using the specified identifier, title, optionality and answer format.
- 
- @param identifier    The string that identifies the form item, which should be unique within the form step.
- @param text          The text displayed as a prompt for the form item's question.
- @param answerFormat  The answer format for the form item.
- @param optional      A Boolean that determines whether the item is optional
- 
- @return An initialized form item.
- */
+    
+    /**
+     Returns an initialized form item using the specified identifier, title, optionality and answer format.
+     
+     @param identifier    The string that identifies the form item, which should be unique within the form step.
+     @param text          The text displayed as a prompt for the form item's question.
+     @param answerFormat  The answer format for the form item.
+     @param optional      A Boolean that determines whether the item is optional
+     
+     @return An initialized form item.
+     */
 - (instancetype)initWithIdentifier:(NSString *)identifier
                               text:(nullable NSString *)text
                       answerFormat:(nullable ORKAnswerFormat *)answerFormat
                           optional:(BOOL) optional;
-
-/**
- Returns an initialized form item using the specified section title.
- 
- @param sectionTitle   The title of the section.
- 
- @return An initialized form item for use as a section header in a form.
- */
+    
+    /**
+     Returns an initialized form item using the specified identifier, title, hidden and answer format.
+     
+     @param identifier    The string that identifies the form item, which should be unique within the form step.
+     @param text          The text displayed as a prompt for the form item's question.
+     @param answerFormat  The answer format for the form item.
+     @param hidden        A Boolean that determines wether the question is hidden by default.
+     
+     @return An initialized form item.
+     */
+- (instancetype)initWithIdentifier:(NSString *)identifier
+                              text:(nullable NSString *)text
+                      answerFormat:(nullable ORKAnswerFormat *)answerFormat
+                            hidden:(BOOL) hidden;
+    
+    /**
+     Returns an initialized form item using the specified identifier, title, optionality, display status and answer format.
+     
+     @param identifier    The string that identifies the form item, which should be unique within the form step.
+     @param text          The text displayed as a prompt for the form item's question.
+     @param answerFormat  The answer format for the form item.
+     @param optional      A Boolean that determines whether the item is optional
+     @param hidden        A Boolean that determines wether the question is hidden by default.
+     
+     @return An initialized form item.
+     */
+- (instancetype)initWithIdentifier:(NSString *)identifier
+                              text:(nullable NSString *)text
+                      answerFormat:(nullable ORKAnswerFormat *)answerFormat
+                          optional:(BOOL) optional
+                            hidden: (BOOL) hidden;
+    
+    /**
+     Returns an initialized form item using the specified section title.
+     
+     @param sectionTitle   The title of the section.
+     
+     @return An initialized form item for use as a section header in a form.
+     */
 - (instancetype)initWithSectionTitle:(nullable NSString *)sectionTitle;
-
-/**
- A string that identifies the form item.
- 
- The identifier should be unique within the form step that contains the form
- item. The identifier is reproduced in the question result object (`ORKQuestionResult`)
- generated for the item.
- */
-@property (nonatomic, copy, readonly) NSString *identifier;
-
-/**
- A Boolean value indicating whether the form item is optional.
- 
- The default value of this property is `YES`. When the value is `YES`, this form item doesn't need
- to be answered for the Continue button of a step form to be enabled.
- */
-@property (nonatomic, getter=isOptional) BOOL optional;
-
-/**
- A localized string that describes the form item.
- 
- If the descriptive text is sufficiently short, you can display it as a prompt next to the item.
- If it is too long to display next to the item, you can display it above the item.
- */
-@property (nonatomic, copy, readonly, nullable) NSString *text;
-
-/**
- A localized string that displays placeholder information for the form item.
- 
- You can display placeholder text in a text field or text area to help users understand how to answer the
- item's question. A placeholder string is not appropriate for choice-based answer formats.
- */
-@property (nonatomic, copy, nullable) NSString *placeholder;
-
-/**
- The format of the expected answer for the item.
- 
- The answer format specifies the type of answer that is expected, in addition to any
- constraints on valid answers.
- 
- By default, the answer format is `nil`. The answer format for a form item that represents a section
- header is always `nil`, because no answer is expected.
- */
-@property (nonatomic, copy, readonly, nullable) ORKAnswerFormat *answerFormat;
-
-/**
+    
+    /**
+     A string that identifies the form item.
+     
+     The identifier should be unique within the form step that contains the form
+     item. The identifier is reproduced in the question result object (`ORKQuestionResult`)
+     generated for the item.
+     */
+    @property (nonatomic, copy, readonly) NSString *identifier;
+    
+    /**
+     A Boolean value indicating whether the form item is optional.
+     
+     The default value of this property is `YES`. When the value is `YES`, this form item doesn't need
+     to be answered for the Continue button of a step form to be enabled.
+     */
+    @property (nonatomic, getter=isOptional) BOOL optional;
+    
+    /**
+     A localized string that describes the form item.
+     
+     If the descriptive text is sufficiently short, you can display it as a prompt next to the item.
+     If it is too long to display next to the item, you can display it above the item.
+     */
+    @property (nonatomic, copy, readonly, nullable) NSString *text;
+    
+    /**
+     A localized string that displays placeholder information for the form item.
+     
+     You can display placeholder text in a text field or text area to help users understand how to answer the
+     item's question. A placeholder string is not appropriate for choice-based answer formats.
+     */
+    @property (nonatomic, copy, nullable) NSString *placeholder;
+    
+    /**
+     The format of the expected answer for the item.
+     
+     The answer format specifies the type of answer that is expected, in addition to any
+     constraints on valid answers.
+     
+     By default, the answer format is `nil`. The answer format for a form item that represents a section
+     header is always `nil`, because no answer is expected.
+     */
+    @property (nonatomic, copy, readonly, nullable) ORKAnswerFormat *answerFormat;
+    
+    /**
      A boolean value indicating wether the question is initially hidden by default.
      
      The default value of this property is `NO`. When `YES` the question will be hidden in the form
      and displayed when the user taps the form item.
- */
-@property (nonatomic, getter=isHidden) BOOL hidden;
+     */
+    @property (nonatomic, getter=isHidden) BOOL hidden;
     
-/**
- Returns an form item that can be used for confirming a text entry.
- 
- This form item is intended to be used with an `ORKFormStep` in order to confirm a previous
- formItem input. Example usage includes a password or participant identifier that is used to
- anonymously identify a study participant.
- 
- Currently, only `ORKTextAnswerFormat` is supported. Unsupported `ORKAnswerFormat` types will
- throw an exception.
- 
- The answer format for this item produces an `ORKBooleanQuestionResult` object.
- 
- @param identifier      The identifier for the `ORKFormItem` that is returned.
- @param text            The text for the `ORKFormItem` that is returned.
- @param errorMessage    The error message to display if the fields do not match
- 
- @return                An `ORKFormItem` with the indicated identifier and text and an ORKAnswerFormat 
-                        that is appropriate for confirming the input form item.
- 
- */
+    /**
+     Returns an form item that can be used for confirming a text entry.
+     
+     This form item is intended to be used with an `ORKFormStep` in order to confirm a previous
+     formItem input. Example usage includes a password or participant identifier that is used to
+     anonymously identify a study participant.
+     
+     Currently, only `ORKTextAnswerFormat` is supported. Unsupported `ORKAnswerFormat` types will
+     throw an exception.
+     
+     The answer format for this item produces an `ORKBooleanQuestionResult` object.
+     
+     @param identifier      The identifier for the `ORKFormItem` that is returned.
+     @param text            The text for the `ORKFormItem` that is returned.
+     @param errorMessage    The error message to display if the fields do not match
+     
+     @return                An `ORKFormItem` with the indicated identifier and text and an ORKAnswerFormat
+     that is appropriate for confirming the input form item.
+     
+     */
 - (ORKFormItem *)confirmationAnswerFormItemWithIdentifier:(NSString *)identifier
                                                      text:(nullable NSString *)text
                                              errorMessage:(NSString *)errorMessage;
-
-@end
+    
+    @end
 
 NS_ASSUME_NONNULL_END
+
+
