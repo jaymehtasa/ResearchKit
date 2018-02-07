7f15a712e75f37bb1d4db7366b445850e99958a4
Yaseen Majeed <majeed@trialx.com>
2018-02-07 16:49:20 +0530

    Including Web2RKdependency in Researchit

---
+++ Web2RKCommon/Web2RKCommon/Const.swift
@@ -0,0 +1,33 @@
+//
+//  Const.swift
+//  Web To RK Template
+//
+//  Created by Zaid Pathan on 25/07/17.
+//  Copyright © 2017 Applied Informatics. All rights reserved.
+//
+
+import Foundation
+
+public struct Const {
+    public struct Defaults {
+        public static let appFirstOpenedDate = "appFirstOpenedDate"
+    }
+    
+    public struct Plist {
+        public static let expiration_period = "expiration_period"
+        public static let has_expiration = "has_expiration"
+        public static let ABFontFamily = "ABFontFamily"
+    }
+    
+    public struct FileName {
+        public static let info = "Info"
+    }
+    
+    public struct FileExtension {
+        public static let plist = "plist"
+    }
+    
+    public struct ContactDetails {
+        public static let mail = "priya@trialx.com"
+    }
+}
